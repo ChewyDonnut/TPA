133deef1ad01fe2c9c3026019e6172f22ce04ce7
ChewyDonnut <ignacioalejandro.vega@alumnos.ulagos.cl>
2023-04-28 03:16:05 -0400

    28/04/23

--- tareas/alrevez.py
+++ tareas/alrevez.py
@@ -7,24 +7,31 @@ class Reves(QMainWindow):
         self.setWindowTitle("girar")
         self.setFixedSize(400,500)
         caja=QVBoxLayout()
+      
         self.texto=QLabel("ingrese palabra y se da vuelta")
         self.dato=QLineEdit("a")
         self.boton=QPushButton("voltear")
+      
         self.boton.clicked.connect(self.girar)    
+      
         caja.addWidget(self.texto)
         caja.addWidget(self.dato)
         caja.addWidget(self.boton)
+      
         ventana=QWidget()
         ventana.setLayout(caja)
         self.setCentralWidget(ventana)
+    
     def girar(self):
         palabra=self.dato.text()
         invertido=" "
         for i in palabra:
             invertido=i + invertido
             self.texto.setText(invertido)
+
+
 if __name__=="__main__":
     app=QApplication(sys.argv)
     ventana=Reves()
     ventana.show()
-    sys.exit(app.exec())#
+    sys.exit(app.exec())
